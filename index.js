b8377f588e39ed3f80cac1e6fec4d857ed1fba9e
noahata <hiabhiyu@gmail.com>
2026-06-20 12:37:20 +0300

    Remove SPONSOR_CHANNEL variable

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const DEVELOPER_CONTACT = '@Ace_spy';
 const MAX_FILE_SIZE_MB = 300;
 
 // ============ SPONSOR CONFIG ============
-const SPONSOR_CHANNEL = 'green apple';
+
 const SPONSOR_LINK = 'https://t.me/GreenAppletgBot/play?startapp=6596414316';
 
 // ============ EXPRESS SETUP ============
